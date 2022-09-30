e696277c435aa447b572a531f176a3185a8ce94b
cody0-0 <113670708+cody0-0@users.noreply.github.com>
2022-09-30 14:18:05 +0200

    Version 1.0.0 Main Window X and Y

--- src/main/java/de/swt/ui/MainWindow.java
+++ src/main/java/de/swt/ui/MainWindow.java
@@ -9,7 +9,28 @@ import java.util.EventListener;
 
 public class MainWindow extends Frame implements EventListener {
 
-        public MainWindow(){
+    private int x;
+    private int y;
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public MainWindow(){
             setSize(500,300);
             setTitle("AWT DEMO");
 
@@ -19,12 +40,13 @@ public class MainWindow extends Frame implements EventListener {
     public void paint(Graphics g) {
         Ellipse2D ellipse2D;
         ellipse2D = new Ellipse2D.Float(
-                10.0F, 10.0F,
+                (float) x, (float) y,
                 5.0F, 5.0F);
         Graphics2D gd2 = (Graphics2D) g;
         gd2.draw(ellipse2D);
         this.repaint();
     }
+
 
     public void showFrame(){
             this.setVisible(true);
